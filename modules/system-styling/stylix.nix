902a2e97955a3f6c7a394db34f94b1e61c61758d
Andy Baizon <dev@baizon.uk>
2024-12-13 21:09:18 +0000

    feat: system styling

---
+++ modules/system-styling/stylix.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: {
+  stylix = {
+    enable = true;
+    image = ./dune.png;
+    polarity = "dark";
+
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+
+    fonts = {
+      serif = {
+        package = pkgs.libre-baskerville;
+        name = "Libre Baskerville";
+      };
+      sansSerif = {
+        package = pkgs.ubuntu-sans;
+        name = "Ubuntu Sans";
+      };
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        applications = 12;
+        terminal = 14;
+      };
+    };
+
+    cursor = {
+      package = pkgs.banana-cursor;
+      name = "Banana";
+      size = 32;
+    };
+  };
+}
